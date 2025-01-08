96014e9bbe633904c1c30a0ed02907477796c09b
Bshisia <shisiabrian7@gmail.com>
2025-01-08 18:53:55 +0300

    chore: validate every room exists in the farm

--- main.go
+++ main.go
@@ -122,3 +122,18 @@ func (af *AntFarm) ParseRoom (line string, isStart bool) error {
 	af.rooms[name] = room
 	return nil
 }
+
+//parslink parses a link line and adds the conections
+func (af *AntFarm) Parselink(line string) error {
+	parts := strings.Split(line, "-")
+	if len(parts) != 2 {
+		return fmt.Errorf("ERROR: invalid data format, invalid link format")
+	}
+
+	room1, exists1 := af.rooms[parts[0]]
+	room2, exists2 := af.rooms[parts[1]]
+
+	if !exists1 || !exists2 {
+		return  fmt.Errorf("ERROR: invalid data format, link to unknown room")
+	}
+}
