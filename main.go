6e3420c754aa59552e9da2fe80ce5763e9000b21
bshisia <shisiabrian7@gmail.com>
2025-01-08 15:07:14 +0300

    feat: implement feat to read the input

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 // Room represent a node in the ant farm
 type Room struct {
 	name        string
@@ -9,18 +14,29 @@ type Room struct {
 	connections []*Room
 }
 
-//Ant farm represents the whole colony 
+// Ant farm represents the whole colony
 type AntFarm struct {
-	rooms map[string]*Room
+	rooms     map[string]*Room
 	startRoom *Room
-	endRoom	*Room
-	numAnts int
-	paths [][]string
+	endRoom   *Room
+	numAnts   int
+	paths     [][]string
 }
 
-//initialise a new ant farm
+// initialise a new ant farm
 func NewAntFarm() *AntFarm {
 	return &AntFarm{
 		rooms: make(map[string]*Room),
 	}
+}
+
+// validate the input file
+func (af *AntFarm) ParseInput(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return fmt.Errorf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	return nil
 }
