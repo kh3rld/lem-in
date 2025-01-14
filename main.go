48efdad321d4765ef2b071927e39203a2676f693
garveyshah <garveyshah1595@gmail.com>
2025-01-14 14:48:21 +0300

    [ParseLink] - checked for dulicate linked and unkworn room

--- main.go
+++ main.go
@@ -157,4 +157,19 @@ func (af *AntFarm) Parselink(line string) error {
 	room1, exists1 := af.rooms[parts[0]]
 	room2, exists2 := af.rooms[parts[1]]
 
+	if !exists1 || !exists2 {
+		return fmt.Errorf("ERROR: invalid data format, link to unkown room")
+	}
+
+	for _, conn := range room1.connections {
+		if conn == room2 {
+			return fmt.Errorf("ERROR: invalid data format, duplicate link")
+		}
+	}
+
+	room1.connections = append(room1.connections, room2)
+	room2.connections = append(room2.connections, room1)
+	return nil
+
 }
+
