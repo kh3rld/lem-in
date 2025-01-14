0b0bb02e65352ddc171d0dbcf28f4dae8091a389
garveyshah <garveyshah1595@gmail.com>
2025-01-14 15:21:52 +0300

    Edmonds-karp] - finding all the paths

--- main.go
+++ main.go
@@ -177,5 +177,33 @@ func (af *AntFarm) Parselink(line string) error {
 func (af *AntFarm) EdmondsKarp() {
 	// Create residual graph
 	residualGraph := make(map[string]map[string]int)
-	
+
+	// Initialize residual graph
+	for name, room := range af.rooms {
+		residualGraph[name] = make(map[string]int)
+		for _, conn := range room.connections {
+			residualGraph[name][conn.name] = 1 // Initial capacity of 1 for each edge
+		}
+	}
+
+	af.paths = make([][]string, 0)
+
+	// Keep finding paths until no more paths exist
+	for {
+		path := af.bfs(residualGraph)
+		if len(path) == 0 {
+			break
+		}
+		af.paths = append(af.paths, path)
+
+		// Update residual graph
+		for i := 0; i < len(path)-1; i++ {
+			u, v := path[i], path[i+1]
+			residualGraph[u][v]--		// Decrease forward edge
+			if residualGraph[v][u] == 0 { // Add reverse edge if it doesn't exist
+				residualGraph[v][u] = 1
+			}
+		}
+	}
 }
+
