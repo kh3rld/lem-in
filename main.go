ec5186908324d5214b31f9ecf6ef2ae54b6581cd
garveyshah <garveyshah1595@gmail.com>
2025-01-14 13:32:27 +0300

    [ParseRoom] - add the room nodes into the antFarm struct type and checking for duplicate start and end rooms

--- main.go
+++ main.go
@@ -100,7 +100,7 @@ func (af *AntFarm) ParseInput(filename string) (string, error) {
 }
 
 // Validates and Parses Room data into the relevant data structures
-func (as *AntFarm) ParseRoom(line string, isStart bool, isEnd bool) error {
+func (af *AntFarm) ParseRoom(line string, isStart bool, isEnd bool) error {
 	parts := strings.Fields(line)
 	if len(parts) != 3 {
 		return fmt.Errorf("ERROR: invalid data format, invalid room defination")
@@ -112,25 +112,37 @@ func (as *AntFarm) ParseRoom(line string, isStart bool, isEnd bool) error {
 	}
 
 	x, err := strconv.Atoi(parts[1])
-	if err !=  nil {
+	if err != nil {
 		return fmt.Errorf("ERROR: invalid data format, invalid x coordinate")
 	}
-
 
 	y, err := strconv.Atoi(parts[2])
 	if err != nil {
 		return fmt.Errorf("ERROR: invalid data format, invalid y coordinate")
 	}
 
-	room := &Room {
-		name: name, 
-		x: x,
-		y: y,
-		isStart: isStart,
-		isEnd: isEnd,
+	room := &Room{
+		name:        name,
+		x:           x,
+		y:           y,
+		isStart:     isStart,
+		isEnd:       isEnd,
 		connections: make([]*Room, 0),
 	}
+	if isStart {
+		if af.startRoom != nil {
+			return fmt.Errorf("ERROR: invalid data format, multiple start rooms")
+		}
+		af.startRoom = room
+	}
+	
+	if isEnd {
+		if af.endRoom != nil {
+			return fmt.Errorf("ERROR: invalid data format, multiple end rooms")
+		}
+		af.endRoom = room
+	}
+
+	af.rooms[name] = room
 	return nil
 }
-
-
