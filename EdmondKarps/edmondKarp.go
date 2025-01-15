e7d881b4ddeca2c97b9ef70cfaedbfa4dc35268f
oumaoumag <ouma.godwin10@gmail.com>
2025-01-15 06:19:29 +0300

    [edm karp] - edmond karp

---
+++ EdmondKarps/edmondKarp.go
@@ -0,0 +1,35 @@
+package edmondskarp
+
+// EdmondsKarp implements the Edmonds-Karp algorithm to find augmenting paths
+func (af *AntFarm) EdmondsKarp() {
+	// Create residual graph
+	residualGraph := make(map[string]map[string]int)
+
+	// Initialize residual graph
+	for name, room := range af.rooms {
+		residualGraph[name] = make(map[string]int)
+		for _, conn := range room.connections {
+			residualGraph[name][conn.name] = 1 // Initial capacity of 1 for each edge
+		}
+	}
+
+	af.paths = make([][]string, 0)
+
+	// Keep finding paths until no more paths exist
+	for {
+		path := af.bfs(residualGraph)
+		if len(path) == 0 {
+			break
+		}
+		af.paths = append(af.paths, path)
+
+		// Update residual graph
+		for i := 0; i < len(path)-1; i++ {
+			u, v := path[i], path[i+1]
+			residualGraph[u][v]--         // Decrease forward edge
+			if residualGraph[v][u] == 0 { // Add reverse edge if it doesn't exist
+				residualGraph[v][u] = 1
+			}
+		}
+	}
+}
