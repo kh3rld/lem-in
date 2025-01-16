4ac4df0f341edfa76f3d0d1b16c3136552e0ec70
garveyshah <garveyshah1595@gmail.com>
2025-01-16 14:06:46 +0300

    add test-cases

--- internal/max_flow_test.go
+++ internal/max_flow_test.go
@@ -6,4 +6,16 @@ func Testbfs(t *testing.T) {
 }
 
 func TestEdmondsKarp(t *testing.T) {
+	tests := []struct {
+		name string
+		graph map[string]map[string]int
+		startRoom string
+		endRoom	string
+		expectedPath []string
+	}{
+		{"Simple Path", map[string]map[string]int{"start": {"A": 1}, "A": {"end": 1}, "end": {},}, "start", "end", []string{"start", "A", "end"},},
+		{"No Path Available", map[string]map[string]int{"start": {"A": 0}, "A": {"end": 0}, "end": {},}, "start", "end", []string{},},
+		{"Multiple Possible Paths", map[string]map[string]int{"start": {"A": 1, "B": 1}, "A": {"end": 1}, "end": {},}, "start", "end", []string{"start", "A", "end"}, }, // BFS will find this path first
+	}
+	
 }
