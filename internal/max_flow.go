329b1c6af77ad25528b0492ed37f02a62962acc2
garveyshah <garveyshah1595@gmail.com>
2025-01-16 16:43:22 +0300

    [Edmonds-Karp] - finding paths and updating the residual graph

--- internal/max_flow.go
+++ internal/max_flow.go
@@ -14,7 +14,25 @@ func (af *AntFarm) EdmondsKarp() {
 		}
 	}
 
-	
+	af.paths = make([][]string, 0)
+
+	// Keep finding paths until no more paths exist
+	for {
+		path := af.bfs(residualGraph)
+		if len(path) == 0 {
+			break
+		}
+		af.paths = append(af.paths, path)
+
+		// Update residual graph
+		for i := 0; i < len(path)-1; i++ {
+			u, v := path[i], path[i+1]
+			residualGraph[u][u]--		 // Decrease forward edge
+			if residualGraph[v][u] == 0 { // Add reverse edge if it doesn't exist
+				residualGraph[v][u] = 1
+			}
+		}
+	}
 }
 
 // Bfs implements breath-first search to find shortest augmenting path
