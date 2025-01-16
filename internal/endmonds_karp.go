adf2ab9a6f9fd6344bd6f01da1dfcd26ff351abb
garveyshah <garveyshah1595@gmail.com>
2025-01-16 13:32:33 +0300

    [feat][bfs] - add function signature

--- internal/endmonds_karp.go
+++ internal/endmonds_karp.go
@@ -5,3 +5,8 @@ package internal
 func (af *AntFarm) EdmondsKarp() {
 	// Function signature
 }
+
+// Bfs implements breath-first search to find shortest augmenting path
+func (af *AntFarm) bfs(residualGraph map[string]map[string]int) []string {
+	// function signature
+}
