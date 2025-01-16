e2d56a012f0e82923403a2b3d29d6998b461d343
garveyshah <garveyshah1595@gmail.com>
2025-01-16 16:05:16 +0300

    [Testing][EdmondsKarp] - add test cases

--- internal/max_flow_test.go
+++ internal/max_flow_test.go
@@ -7,23 +7,23 @@ import (
 
 func TestBfs(t *testing.T) {
 	tests := []struct {
-		name string
-		graph map[string]map[string]int
-		startRoom string
-		endRoom	string
+		name         string
+		graph        map[string]map[string]int
+		startRoom    string
+		endRoom      string
 		expectedPath []string
 	}{
-		{"Simple Path", map[string]map[string]int{"start": {"A": 1}, "A": {"end": 1}, "end": {},}, "start", "end", []string{"start", "A", "end"},},
-		{"No Path Available", map[string]map[string]int{"start": {"A": 0}, "A": {"end": 0}, "end": {},}, "start", "end", []string{},},
-		{"Multiple Possible Paths", map[string]map[string]int{"start": {"A": 1, "B": 1}, "A": {"end": 1}, "end": {},}, "start", "end", []string{"start", "A", "end"}, }, // BFS will find this path first
+		{"Simple Path", map[string]map[string]int{"start": {"A": 1}, "A": {"end": 1}, "end": {}}, "start", "end", []string{"start", "A", "end"}},
+		{"No Path Available", map[string]map[string]int{"start": {"A": 0}, "A": {"end": 0}, "end": {}}, "start", "end", []string{}},
+		{"Multiple Possible Paths", map[string]map[string]int{"start": {"A": 1, "B": 1}, "A": {"end": 1}, "end": {}}, "start", "end", []string{"start", "A", "end"}}, // BFS will find this path first
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			af := &AntFarm{
-				rooms: make(map[string]*Room),
+				rooms:     make(map[string]*Room),
 				startRoom: &Room{name: tt.startRoom},
-				endRoom: &Room{name: tt.endRoom},
+				endRoom:   &Room{name: tt.endRoom},
 			}
 
 			path := af.bfs(tt.graph)
@@ -34,7 +34,16 @@ func TestBfs(t *testing.T) {
 	}
 }
 
-// func TestEdmondsKarp(t *testing.T) {
-	
-// }
-
+func TestEdmondsKarp(t *testing.T) {
+	tests := []struct {
+		name          string
+		rooms         map[string]*Room
+		startRoom     string
+		endRoom       string
+		connections   map[string][]string
+		expectedPaths [][]string
+	}{
+		{"Simple Linear Path", map[string]*Room{"start" :{},"A": {},"end": {},}, "start","end", map[string][]string{"start": {"A"}, "A": {"end"},}, [][]string{{"start", "A", "end"},},},
+		{"Two Parallel Paths", map[string]*Room{"start": {}, "A" : {}, "B": {}, "end": {},}, "start", "end", map[string][]string{"start": {"A", "B"}, "A": {"end"}, "B": {"end"}, },[][]string{{"start", "A", "end"}, {"start", "B", "end"},},},
+	}
+}
