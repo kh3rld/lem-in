b156e72203e150f3714b5ffc263e9048be89c789
garveyshah <garveyshah1595@gmail.com>
2025-01-16 14:11:48 +0300

    [Testing] - full implementation of test-bfs

--- internal/max_flow_test.go
+++ internal/max_flow_test.go
@@ -1,6 +1,9 @@
 package internal
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Testbfs(t *testing.T) {
 }
@@ -16,6 +19,21 @@ func TestEdmondsKarp(t *testing.T) {
 		{"Simple Path", map[string]map[string]int{"start": {"A": 1}, "A": {"end": 1}, "end": {},}, "start", "end", []string{"start", "A", "end"},},
 		{"No Path Available", map[string]map[string]int{"start": {"A": 0}, "A": {"end": 0}, "end": {},}, "start", "end", []string{},},
 		{"Multiple Possible Paths", map[string]map[string]int{"start": {"A": 1, "B": 1}, "A": {"end": 1}, "end": {},}, "start", "end", []string{"start", "A", "end"}, }, // BFS will find this path first
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			af := &AntFarm{
+				rooms: make(map[string]*Room),
+				startRoom: &Room{name: tt.startRoom},
+				endRoom: &Room{name: tt.endRoom},
+			}
+
+			path := af.bfs(tt.graph)
+			if !reflect.DeepEqual(path, tt.expectedPath) {
+				t.Errorf("bfs() = %v, want %v", path, tt.expectedPath)
+			}
+		})
 	}
 	
 }
