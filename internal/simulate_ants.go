356118ce726e0bad7e6421ff6d2e6b576117da83
Bshisia <shisiabrian7@gmail.com>
2025-01-17 07:31:49 +0300

    chore: sort the paths by length

--- internal/simulate_ants.go
+++ internal/simulate_ants.go
@@ -1,7 +1,14 @@
 package internal
 
+import "sort"
+
 func (af *AntFarm) SimulateAnts() []string {
 	if len(af.paths) == 0 {
 		return nil
 	}
+
+	//sort paths by length
+	sort.Slice(af.paths, func(i, j int) bool {
+		return len(af.paths[i]) < len(af.paths[j])
+	})
 }
